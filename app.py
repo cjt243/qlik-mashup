2593e41133e5562ef942d434da1ae262007b6bb5
Cullin Tripp <fof@qlik.com>
2020-01-30 16:07:23 -0500

    Initial commit - not working.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("main.html")
+
+@app.route("/cullin")
+def salvador():
+    return "Hello, Cullin"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+var config = {
+    host: "win-h4etcap5m3h", //the address of your Qlik Engine Instance
+    prefix: "/", //the virtual proxy to be used. for example "/anonymous/"
+    port: "443", //the port to be used if different from the current port
+    isSecure: true //should be true if connecting over HTTPS
+};
+
+require.config({
+    baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port: "") + config.prefix + "resources",
+  });
+
+  require(['js/qlik'], function(qlik) {
+      //if there's any error we catch it here
+      qlik.setOnError( function ( error ) {
+          console.log(error);
+      });
+  });
+
+var app = qlik.openApp('ffa1241e-9e27-4d7b-9eaf-70feeca6edb2', config);
