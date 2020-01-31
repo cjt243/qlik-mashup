719efba3083e0e8d58c3eb4aa05dfce2857c6e10
Cullin Tripp <fof@qlik.com>
2020-01-31 12:56:49 -0500

    disable CORS handling

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/cullin")
-def salvador():
-    return "Hello, Cullin"
 
 if __name__ == "__main__":
     app.run(debug=True)
