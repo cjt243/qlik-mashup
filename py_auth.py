63b17957f9cd0e93f9411d046aa8821e1065c2c4
Cullin Tripp <fof@qlik.com>
2020-02-26 11:46:20 -0500

    Added hostname to local hostfile - no longer need to use IP in call

--- py_auth.py
+++ py_auth.py
@@ -7,19 +7,19 @@ key_file = '/mnt/c/Users/fof/Documents/VM-Auth/Qlik-Cert-Laptop/client_key.pem'
 cert = (cert_file,key_file)
 
 def _getTicket(url):
-    payload = "\r\n{\r\n  \"UserDirectory\": \"192.168.80.166\",\r\n  \"UserId\": \"cullin\"\r\n}"
+    payload = "\r\n{\r\n  \"UserDirectory\": \"WIN-H4ETCAP5M3H\",\r\n  \"UserId\": \"cullin\"\r\n}"
     headers = {
       'x-Qlik-Xrfkey': 'abcdefghijklmnop',
       'Content-Type': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data = payload, cert=cert, verify=False)
-
+    # print(response.json()['Ticket'])
     return response.json()['Ticket']
 
 def getSession():
-    ticket = _getTicket("https://192.168.80.166:4243/qps/mashup/ticket?Xrfkey=abcdefghijklmnop")
-    url = "https://192.168.80.166/mashup/qrs/about?Xrfkey=abcdefghijklmnop&Qlikticket={}".format(ticket)
+    ticket = _getTicket("https://WIN-H4ETCAP5M3H.tripp.lan:4243/qps/mashup/ticket?Xrfkey=abcdefghijklmnop")
+    url = "https://WIN-H4ETCAP5M3H.tripp.lan/mashup/qrs/about?Xrfkey=abcdefghijklmnop&Qlikticket={}".format(ticket)
 
     payload  = {}
     headers = {
