1cec507e1fb5275a8e5a32727bcf34713da4e01c
Cullin Tripp <fof@qlik.com>
2020-02-26 11:44:44 -0500

    Setting cookie domain

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, make_response
 from py_auth import *
-# from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 @app.route("/")
 def home():
     resp = make_response(render_template("index.html"))
-    resp.set_cookie('X-Qlik-Session-Mashup',getSession(),samesite=None)
+    resp.set_cookie('X-Qlik-Session-Mashup',getSession(),domain='.tripp.lan',samesite=False)
     return resp
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
